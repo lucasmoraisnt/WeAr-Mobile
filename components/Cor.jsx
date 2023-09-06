666e17b9315022d57494bb8f6336d6d3bc98fd99
lucasmoraist <luksmnt1101@gmail.com>
2023-09-06 15:08:57 -0300

    Finalizando o componente de cores

--- components/Cor.jsx
+++ components/Cor.jsx
@@ -1,17 +1,52 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const Cor = () => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Cor: Preto</Text>
-            <View>
-                <View/>
-                <View/>
-                <View/>
+            <View style={styles.cores}>
+                <View style={styles.circulo1}/>
+                <View style={styles.circulo2}/>
+                <View style={styles.circulo3}/>
             </View>
         </View>
     )
 }
 
 export default Cor;
+
+const styles = StyleSheet.create({
+    container:{
+        marginLeft: 15
+    },
+    cores:{
+        flexDirection: 'row',
+        paddingVertical: 10,
+        gap: 8
+    },
+    circulo1: {
+        width: 40,
+        height: 40,
+        borderWidth: 1,
+        borderColor: 'rgba(153,152,152,1)',
+        borderRadius: 30,
+        backgroundColor: "#000"
+    },
+    circulo2: {
+        width: 40,
+        height: 40,
+        borderWidth: 1,
+        borderColor: 'rgba(153,152,152,1)',
+        borderRadius: 30,
+        backgroundColor: "#fff"
+    },
+    circulo3: {
+        width: 40,
+        height: 40,
+        borderWidth: 1,
+        borderColor: 'rgba(153,152,152,1)',
+        borderRadius: 30,
+        backgroundColor: "#194c30"
+    }
+})
